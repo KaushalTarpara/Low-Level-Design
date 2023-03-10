5fb2813e4a1ac4687db30075d0d7e9fef4c36488
KdTarpara <kaushaltarpara10702@gmail.com>
2023-03-10 22:36:51 +0530

    Add Singleton Pattern Demo

---
+++ src/DesignPattern/Singleton/Singleton.java
@@ -0,0 +1,25 @@
+package DesignPattern.Singleton;
+
+public class Singleton {
+    //data member
+    static Singleton instance = null;
+
+    //Constructor private
+    private Singleton(){
+
+    }
+
+    //
+    static Singleton getInstance(){
+        if(instance==null){
+            instance =new Singleton();
+        }
+        return instance;
+    }
+
+    //Demo Method
+    public void sayHello(){
+        System.out.println("Singleton Class");
+        System.out.println(instance);
+    }
+}
