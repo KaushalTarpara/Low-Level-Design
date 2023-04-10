e9393042a02dad24d391160f1dac4e95306298a5
KdTarpara <kaushaltarpara10702@gmail.com>
2023-04-10 06:59:37 +0530

    Add 2 Behavioral Design Pattern (Chain of Responsibility and Observer

---
+++ src/BehavioralDesignPattern/Observer/Listener/EventListener.java
@@ -0,0 +1,7 @@
+package BehavioralDesignPattern.Observer.Listener;
+
+import java.io.File;
+
+public interface EventListener {
+    void update(String eventType, File file);
+}
---
+++ src/BehavioralDesignPattern/Observer/Listener/LogOpenListener.java
@@ -0,0 +1,20 @@
+package BehavioralDesignPattern.Observer.Listener;
+
+
+import BehavioralDesignPattern.Observer.Listener.EventListener;
+
+import java.io.File;
+
+public class LogOpenListener implements EventListener {
+    private File log;
+
+    public LogOpenListener(String fileName) {
+        this.log = new File(fileName);
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println("Save to log " + log + ": Someone has performed "
+                + eventType + " operation with the following file: " + file.getName());
+    }
+}
